a6ddfb58fcc5365484aaecf1093838f855d34249
Gregory Gershkowitz <gershkowitz.3@osu.edu>
2016-12-23 00:45:07 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -10,10 +10,10 @@ import nltk
 
 
 
-ckey = 'T2uRgVz5dw9jaH8bsHBa8WAXJ'
-csecret = 'ADgXbCgMrux4mYzOZgJmn0dBGtKxbAKVgQ9p7poJ6wPfoTyBaB'
-atoken = '307343350-arNaZ2TQ9yyxacSvZzr8bmcVq7GXl92YqBMvxLLA'
-asecret = 'oL6YQOzLbU5iJR5Rwb5wiKrySfutS292RgNzEPo1IQ7Ew'
+ckey = 'XXXXX'
+csecret = 'XXXXXX'
+atoken = 'XXXXXXXX'
+asecret = 'XXXXXXXXX'
 
 class listener(StreamListener):
 
